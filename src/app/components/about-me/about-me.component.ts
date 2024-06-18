e613d3ef7b9badace2e5d2021e16f34190f2c12d
Bruno Bizeray <bbizeray@ciandt.com>
2024-06-18 01:04:11 -0300

    style: add icons white to change on darkmode

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from '../../service/dark-mode.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
 export class AboutMeComponent {
-
+  constructor(public darkModeService: DarkModeService) {}
 }
--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from '../../service/dark-mode.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './about-page.component.html',
-  styleUrl: './about-page.component.scss'
+  styleUrl: './about-page.component.scss',
 })
 export class AboutPageComponent {
-
+  constructor(public darkModeService: DarkModeService) {}
 }
